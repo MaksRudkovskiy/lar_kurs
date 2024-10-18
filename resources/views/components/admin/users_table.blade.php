5865af207a018b6a7002d4bbcef9557a10f04954
Maxon Sheluponev <lissetathegreatsheluponeva@gmail.com>
2024-10-19 00:07:04 +0800

    Commiting for the sake of commiting

--- resources/views/components/admin/users_table.blade.php
+++ resources/views/components/admin/users_table.blade.php
@@ -73,14 +73,33 @@
                         <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-lg whitespace-nowrap p-4 font-bold">
                             {{ $user->role }}
                         </td>
-                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-lg whitespace-nowrap p-4">
+                        <td class="border-t-0 px-6 border-l-0 border-r-0 text-lg whitespace-nowrap p-4">
                             <div class="flex justify-center gap-3">
-                            <form method="POST" action="{{ route('users.privelege', $user->id) }}" style="display:inline;">
-                                @csrf
-                                @method('PUT')
-                                <button type="submit"
-                                    class="text-yellow-700 hover:text-white border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center">Привелегировать</button>
-                            </form>
+                            @if($user->role=="admin")
+                                <form method="POST" action="{{ route('users.privelege', $user->id) }}" style="display:inline;">
+                                    @csrf
+                                    @method('PUT')
+                                    <div
+                                        class=" border border-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300
+                                        font-medium rounded-lg text-sm px-3 py-2.5 text-center">
+                                        Админ
+                                    </div>
+                                </form>
+                            @else
+                                <form method="POST" action="{{ route('users.privelege', $user->id) }}" style="display:inline;">
+                                    @csrf
+                                    @method('PUT')
+                                    <button type="submit"
+                                        class="text-yellow-700 hover:text-white border border-yellow-700 hover:bg-yellow-800 focus:ring-4
+                                        focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center">
+                                        @if($user->role!=="privelegious_user")
+                                            Привелегировать
+                                        @else
+                                            Депривелегировать
+                                        @endif 
+                                    </button>
+                                </form>
+                            @endif
                                 <button onclick="toggleDetails('{{ $user->id }}')"
                                     class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center">
                                     Детали
