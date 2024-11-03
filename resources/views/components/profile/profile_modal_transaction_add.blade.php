953f9b6f386e98b0edf00efbabb166aac4adf515
Maxon Sheluponev <lissetathegreatsheluponeva@gmail.com>
2024-11-03 11:41:23 +0800

    Perfect. Small adjustments needed and basically my course work is done!

--- resources/views/components/profile/profile_modal_transaction_add.blade.php
+++ resources/views/components/profile/profile_modal_transaction_add.blade.php
@@ -9,11 +9,11 @@
                     {{__('profile.add_trans')}}
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">{{__('profile.close_modal')}}</span>
-            </button>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">{{__('profile.close_modal')}}</span>
+                </button>
             </div>
             <div class="p-4 md:p-5 space-y-4">
 
